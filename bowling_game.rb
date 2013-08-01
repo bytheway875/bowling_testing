1f08d8df590e433ce3e9dd4ebc0747a1cd372630
MakerSquare Student <students@themakersquare.com>
2013-08-01 12:26:37 -0500

    Did some refactoring

--- bowling_game.rb
+++ bowling_game.rb
@@ -10,33 +10,33 @@ class BowlingGame
   def score
     @total_score = 0
     @current_roll = 0
-    frame = 1
+    @frame = 1
 
-    while @current_roll < @rolls.size-1
-      @roll = @rolls[@current_roll]
-      @next_roll = @rolls[@current_roll +1]
-      
-
-      @rolls[@current_roll +2] ? @first_roll_next_frame = @rolls[@current_roll +2] : @first_roll_next_frame = 0
-      
-
-      if @roll == 10
-        if frame > 10
-         return @total_score
+    while rolls?
+      set_roll_logic
+      if strike?
+        if @frame > 10
+          return @total_score
         else
           strike_frame
-        end
-      elsif @roll + @next_roll == 10
+      end
+      elsif spare?
         spare_frame
       else
         regular_frame
       end
-      frame += 1
+      @frame += 1
     end
     return @total_score
   end
 
   private
+
+  def set_roll_logic
+    @roll = @rolls[@current_roll]
+    @next_roll = @rolls[@current_roll +1]
+    @rolls[@current_roll +2] ? @first_roll_next_frame = @rolls[@current_roll +2] : @first_roll_next_frame = 0
+  end
 
   def strike_frame
     @total_score += 10 + @next_roll + @first_roll_next_frame
@@ -51,6 +51,22 @@ class BowlingGame
   def regular_frame
     @total_score += @roll + @next_roll
     @current_roll +=2
+  end
+
+  def strike?
+    @roll == 10
+  end
+
+  def spare?
+    @roll + @next_roll == 10
+  end
+
+  def rolls?
+    @current_roll < @rolls.size-1
+  end
+
+  def score_frame
+    
   end
 
 end
